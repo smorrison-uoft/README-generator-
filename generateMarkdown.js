e26a0b3bffcb820c991f915eedf51f6c5beaf241
stephanie morrison <stephmorr.to@gmail.com>
2020-07-29 11:52:19 -0400

    changes2

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,12 +1,23 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   console.log(data);
-    return `# ${data.projectName}`;
+    return `# ${data.projectName}`
+            `# ${data.projectAuthor}`
+            `# ${data.projectInstructions}`
+            `# ${data.Useage}`
+            `# ${data.licenceName}`
+            `# ${data.githubUsername}`
+            `# ${data.emailAddress}`
+            `# ${data.Contributors}`
+            `# ${data.projectInstructions}`
+            `# ${data.Tests}`
+            `# ${data.Questions}`
+            `# ${data.readme}`
+
+
 
   
-            
-
-  }
+}
   
   module.exports = generateMarkdown;
   
